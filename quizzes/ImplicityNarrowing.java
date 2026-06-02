60532d86176d119945d747fabddb91921d260a92
Mohamed Anwer <mhmdlyman362@gmail.com>
2026-06-02 21:10:37 +0200

    add quizzes

---
+++ quizzes/ImplicityNarrowing.java
@@ -0,0 +1,5 @@
+void main() {
+    short s = 10;
+    s += 5.6; // This will make implicit narrowing casting
+    IO.println(s);
+}
---
+++ quizzes/TinaryOperatorTip.java
@@ -0,0 +1,4 @@
+void main() {
+    Object obj = true ? 'A' : 1.0;
+    System.out.println(obj.getClass().getSimpleName());
+}
