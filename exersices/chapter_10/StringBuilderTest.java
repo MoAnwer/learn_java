b1606fbc95a7367e56c2b6fd4b98c80fc7f9795b
Mohamed Anwer <mhmdlyman362@gmail.com>
2026-06-13 05:40:24 +0200

    Refactor StringBuilderTest to include proper class structure and main method

--- exersices/chapter_10/StringBuilderTest.java
+++ exersices/chapter_10/StringBuilderTest.java
@@ -1,16 +1,20 @@
-void main () {
-    StringBuilder s1 = new StringBuilder("Java");
-    StringBuilder s2 = new StringBuilder("HTML");
-    s1.append(" is fun");
-    s1.append(s2);
-    s1.insert(2, "is fun");
-    s1.insert(1, s2);
-    s1.charAt(2);
-    s1.length();
-    s1.deleteCharAt(3);
-    s1.delete(1, 3);
-    s1.reverse();
-    s1.replace(1, 3, "Computer");
-    s1.substring(1, 3);
-    s1.substring(2); 
+package exersices.chapter_10;
+
+public class StringBuilderTest {
+
+    public static void main(String[] args) {
+        StringBuilder s1 = new StringBuilder("Java");
+        StringBuilder s2 = new StringBuilder("HTML");
+        s1.append(" is fun"); // Java is fun
+        s1.append(s2); // Java is funHTML
+        IO.println(s1.insert(2, "is fun")); // Jais funva is funHTML
+        IO.println(s1.insert(1, s2)); // JHTMLais funva is funHTML
+        IO.println(s1.length()); // T
+        IO.println(s1.deleteCharAt(3)); // JHTLais funva is funHTML
+        IO.println(s1.delete(1, 3)); // JLais funva is funHTML
+        IO.println(s1.reverse()); // LMTHnuf si avnuf siaLJ 
+        IO.println(s1.replace(1, 3, "Computer")); // LComputerHnuf si avnuf siaLJ 
+        IO.println(s1.substring(1, 3)); // Co
+        IO.println(s1.substring(2)); // omputerHnuf si avnuf siaLJ
+    }
 }
